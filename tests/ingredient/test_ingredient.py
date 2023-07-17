654b6d045df8d0975d42bf7bacdc1778b19c9cf2
Mark Allen <markallenarchviz@gmail.com>
2023-07-16 23:36:50 -0300

    test_ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+
+    ingrediente = Ingredient("frango")
+    assert ingrediente.name == "frango"
+    assert ingrediente.restrictions == {Restriction.ANIMAL_MEAT,
+                                        Restriction.ANIMAL_DERIVED}
+
+    assert ingrediente.__repr__() == "Ingredient('frango')"
+
+    animal = Ingredient("carne")
+
+    assert hash(ingrediente) != hash(animal)
+    assert hash(ingrediente) == hash(Ingredient("frango"))
+
+    assert (ingrediente == animal) is False
+    assert (ingrediente == ingrediente) is True
