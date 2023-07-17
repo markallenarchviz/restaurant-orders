72c43c09d347017d12364d9fec7a9b63b5c7aa31
Mark Allen <markallenarchviz@gmail.com>
2023-07-16 23:46:58 -0300

    menu_builder fixes

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -40,3 +40,57 @@ class MenuBuilder:
                 dishes.append(dish_menu)
 
         return dishes
+from typing import Dict, List
+
+from services.inventory_control import InventoryMapping
+from services.menu_data import MenuData
+
+DATA_PATH = "data/menu_base_data.csv"
+INVENTORY_PATH = "data/inventory_base_data.csv"
+
+
+class MenuBuilder:
+    def __init__(self, data_path=DATA_PATH, inventory_path=INVENTORY_PATH):
+        self.menu_data = MenuData(data_path)
+        self.inventory = InventoryMapping(inventory_path)
+
+    def make_order(self, dish_name: str) -> None:
+        try:
+            curr_dish = [
+                dish
+                for dish in self.menu_data.dishes
+                if dish.name == dish_name
+            ][0]
+        except IndexError:
+            raise ValueError("Dish does not exist")
+
+        self.inventory.consume_recipe(curr_dish.recipe)
+
+    # Req 4
+    def get_main_menu(self, restriction=None) -> List[Dict]:
+        custom_menu = []
+
+        for dish in self.menu_data.dishes:
+            if not self.inventory.check_recipe_availability(dish.recipe):
+                continue
+
+            clean_dish = self.check_restriction(restriction, dish)
+
+            if clean_dish:
+                custom_menu.append(self.recipe_dict_builder(dish))
+
+        return custom_menu
+
+    def check_restriction(self, restriction, dish):
+        for restrict in dish.get_restrictions():
+            if restriction == restrict:
+                return False
+        return True
+
+    def recipe_dict_builder(self, dish):
+        return {
+            "dish_name": dish.name,
+            "ingredients": dish.get_ingredients(),
+            "price": dish.price,
+            "restrictions": dish.get_restrictions(),
+        }
